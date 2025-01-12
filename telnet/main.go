322ac330091442f704920d43f2b366a8f22ec024
Nik Swap <nikswap@local.host>
2025-01-12 20:11:36 +0100

    First commit of telnet honypot. Still need to save data soemwhere

---
+++ telnet/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func CheckError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	addr := ":2323"
+	server, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer server.Close()
+
+	log.Println("Server is running on:", addr)
+
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			log.Println("Failed to accept conn.", err)
+			continue
+		}
+
+		go func(conn net.Conn) {
+			defer func() {
+				conn.Close()
+			}()
+			n, err := conn.Write([]byte("$"))
+			t := time.Now()
+			CheckError(err)
+			fmt.Println("Bytes written", n)
+			lines_sent := make([]string, 100)
+			//Write something ala: "user>" or something to the client
+			//Maybe write a prompt? like "$"
+			//Keep reading from conn until connection is closed from client
+			buf := make([]byte, 0, 4096)
+			tmp := make([]byte, 256)
+			lb_counter := 0
+			for {
+				n, err := conn.Read(tmp)
+				if err != nil {
+					if err != io.EOF {
+						fmt.Println("read error:", err)
+					}
+					break
+				}
+				buf = append(buf, tmp[:n]...)
+				old_lb := lb_counter
+				for i, c := range buf[lb_counter:] {
+					if c == 0xA {
+						lb_counter = i
+						break
+					}
+				}
+				if old_lb < lb_counter {
+					//conn.Write(buf[old_lb:lb_counter])
+					lines_sent = append(lines_sent, base64.StdEncoding.EncodeToString(buf[old_lb:lb_counter]))
+					fmt.Println(t.Format("2006-01-02 15:04:05"), ",", conn.RemoteAddr().String(), ",", lines_sent[len(lines_sent)-1])
+					conn.Write([]byte("\n$"))
+				}
+			}
+			//Save data in a form as:
+			//Timestamp start, Timestamp end, Client ip, Client port, Length of data recived, Data recived in base64
+			//Write data to a csv file
+			//io.Copy(conn, conn)
+			fmt.Println("Connection done")
+		}(conn)
+	}
+}
